121c55e441049120df74d22a0141e468afebd498
Florian Losch <losch@uni-bonn.de>
2015-05-14 17:46:52 -0600

    vert data added

---
+++ data/vert/mean.R
@@ -0,0 +1,41 @@
+var_l=c('DT_CON','POT_VORTIC','QV','T_ENTR','THHR_RAD','TINC_LH','T','U','V','W')
+
+
+vars=paste(var_l,'_fts',sep='')
+
+for (ii in seq_along(vars)){
+   var_files=list.files(pattern=paste(vars[ii],'_2.*',sep=''))
+   print(var_files)
+   nfiles=length(var_files)
+   load(var_files[1])
+   mean_out=array(0,dim=c(length(prof_vert_fts),nfiles))
+   for (jj in seq_along(var_files)){
+      cat(var_files[jj],'\n')
+      load(var_files[jj])
+      mean_out[,jj]=prof_vert_fts
+   } 
+   mean_out=apply(mean_out,1,mean,na.rm=TRUE)
+   save(mean_out,file=paste(vars[ii],'_mean.RData',sep=''))
+   write.table(mean_out,file=paste(vars[ii],'_mean.csv',sep=''),quote=FALSE
+	       ,row.names=FALSE,col.names=FALSE,sep=';')
+}
+
+vars=paste(var_l,'_ats',sep='')
+
+for (ii in seq_along(vars)){
+   var_files=list.files(pattern=paste(vars[ii],'_2.*',sep=''))
+   print(var_files)
+   nfiles=length(var_files)
+   load(var_files[1])
+   mean_out=array(0,dim=c(length(prof_vert_ats),nfiles))
+   for (jj in seq_along(var_files)){
+       cat(var_files[jj],'\n')
+       load(var_files[jj])
+       mean_out[,jj]=prof_vert_ats
+   } 
+   mean_out=apply(mean_out,1,mean,na.rm=TRUE)
+   save(mean_out,file=paste(vars[ii],'_mean.RData',sep=''))
+   write.table(mean_out,file=paste(vars[ii],'_mean.csv',sep=''),quote=FALSE
+               ,row.names=FALSE,col.names=FALSE,sep=';')
+}
+
